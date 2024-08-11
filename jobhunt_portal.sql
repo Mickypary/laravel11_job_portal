8bf8c85cab2287e372f87fc0836e88a66bf77555
Mickypary <mikipary@gmail.com>
2024-08-11 23:15:12 +0100

    second commit with db

--- jobhunt_portal.sql
+++ jobhunt_portal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 10, 2024 at 11:38 PM
+-- Generation Time: Aug 12, 2024 at 12:14 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -93,6 +93,36 @@ CREATE TABLE IF NOT EXISTS `failed_jobs` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `home_page_items`
+--
+
+CREATE TABLE IF NOT EXISTS `home_page_items` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `heading` text NOT NULL,
+  `text` text DEFAULT NULL,
+  `job_title` text NOT NULL,
+  `job_category` text NOT NULL,
+  `job_location` text NOT NULL,
+  `search` text NOT NULL,
+  `background_image` text NOT NULL,
+  `job_category_heading` text NOT NULL,
+  `job_category_subheading` text DEFAULT NULL,
+  `job_category_status` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `home_page_items`
+--
+
+INSERT INTO `home_page_items` (`id`, `heading`, `text`, `job_title`, `job_category`, `job_location`, `search`, `background_image`, `job_category_heading`, `job_category_subheading`, `job_category_status`, `created_at`, `updated_at`) VALUES
+(1, 'Find Your Desired Job', 'Search the best, perfect and suitable jobs that matches your skills in your expertise area.', 'Job Title', 'Job Category', 'Job Location', 'Search', 'banner_home.jpg', 'Job Categories', 'Get the list of all the popular job categories here', 'Show', NULL, '2024-08-11 19:02:53');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `jobs`
 --
 
@@ -131,6 +161,42 @@ CREATE TABLE IF NOT EXISTS `job_batches` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `job_categories`
+--
+
+CREATE TABLE IF NOT EXISTS `job_categories` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `icon` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `job_categories`
+--
+
+INSERT INTO `job_categories` (`id`, `name`, `icon`, `created_at`, `updated_at`) VALUES
+(1, 'Engineering', 'fas fa-magic', '2024-08-11 16:09:58', '2024-08-11 16:09:58'),
+(2, 'Medical', 'fas fa-stethoscope', '2024-08-11 16:12:00', '2024-08-11 19:15:06'),
+(3, 'Accounting', 'fas fa-landmark', '2024-08-11 16:13:35', '2024-08-11 16:13:35'),
+(4, 'Data Entry', 'fas fa-share-alt', '2024-08-11 16:14:56', '2024-08-11 17:06:12'),
+(5, 'Marketing', 'fas fa-bullhorn', '2024-08-11 16:16:05', '2024-08-11 16:16:05'),
+(6, 'Production', 'fas fa-sitemap', '2024-08-11 16:19:51', '2024-08-11 16:19:51'),
+(7, 'Garments', 'fas fa-users', '2024-08-11 16:21:36', '2024-08-11 16:21:36'),
+(8, 'Education', 'fas fa-user-graduate', '2024-08-11 16:22:54', '2024-08-11 16:22:54'),
+(9, 'Technician', 'fas fa-street-view', '2024-08-11 16:24:21', '2024-08-11 16:24:21'),
+(11, 'Security', 'fas fa-lock', '2024-08-11 19:21:07', '2024-08-11 19:21:07'),
+(12, 'Telecommunication', 'fas fa-vector-square', '2024-08-11 19:24:08', '2024-08-11 19:24:08'),
+(13, 'Commercial', 'fas fa-suitcase', '2024-08-11 19:25:38', '2024-08-11 19:25:38'),
+(14, 'Web Development', 'fas fa-solid fa-terminal', '2024-08-11 20:37:23', '2024-08-11 20:43:22'),
+(15, 'Web Design', 'fas fa-solid fa-code', '2024-08-11 20:37:52', '2024-08-11 20:44:38'),
+(16, 'Customer Support', 'fab fa-brands fa-intercom', '2024-08-11 20:45:36', '2024-08-11 20:47:50');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -139,7 +205,7 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `migrations`
@@ -149,7 +215,10 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '0001_01_01_000000_create_users_table', 1),
 (2, '0001_01_01_000001_create_cache_table', 1),
 (3, '0001_01_01_000002_create_jobs_table', 1),
-(4, '2024_08_09_132027_create_admins_table', 2);
+(4, '2024_08_09_132027_create_admins_table', 2),
+(6, '2024_08_11_095357_create_home_page_items_table', 3),
+(7, '2024_08_11_142139_create_job_categories_table', 4),
+(8, '2024_08_11_221000_create_why_choose_items_table', 5);
 
 -- --------------------------------------------------------
 
@@ -187,9 +256,7 @@ CREATE TABLE IF NOT EXISTS `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('71VcyABA1ZwE7TOoWpbQyLfMU9mdglsvMuJk7PPU', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiUmhXc1FrV3N6RGF3a1dIZ21lUU1LQmNqTDVZWWNrV01lSllaMXpNdiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzI6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbi9ob21lIjt9czo1MjoibG9naW5fYWRtaW5fNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO30=', 1723324947),
-('SWvexlZoJ6cNb1NCC5CdYPTaGZ9DKMUvVMmgGY0u', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiYmZVRkRQWXVIdVloSU44enUxbXJNVlVxNHJBZ2ZJSFpodVdaUHpTVCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTI6ImxvZ2luX2FkbWluXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTt9', 1723325796),
-('yLfGZSyKhqySg70HK24XjNKjDpRzKmFDjLxIngor', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiZDJkclF0QzhFUlpnSjBZUFFhekRVclJlY3dscEl6VmVueUNRWnZNSiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzI6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbi9ob21lIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MjoibG9naW5fYWRtaW5fNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO30=', 1723292717);
+('OgBr5auW86xDI2huMaKovhzasvLOd5ijEccbBLpg', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoid3p2WlFwOEFYekU5eW44Q1dONld2ajBCSjJsbHRDeGxlYWtxNDVaTyI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NDM6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hZG1pbi93aHktY2hvb3NlL3ZpZXciO31zOjUyOiJsb2dpbl9hZG1pbl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7fQ==', 1723414352);
 
 -- --------------------------------------------------------
 
@@ -209,6 +276,31 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `why_choose_items`
+--
+
+CREATE TABLE IF NOT EXISTS `why_choose_items` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `icon` text NOT NULL,
+  `heading` text NOT NULL,
+  `text` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `why_choose_items`
+--
+
+INSERT INTO `why_choose_items` (`id`, `icon`, `heading`, `text`, `created_at`, `updated_at`) VALUES
+(1, 'fas fa-briefcase', 'Quick Apply', 'You can just create your account in our website and apply for desired job very quickly.', '2024-08-11 21:54:56', '2024-08-11 21:54:56'),
+(2, 'fas fa-search', 'Search Tool', 'We provide a perfect and advanced search tool for job seekers, employers or companies.', '2024-08-11 21:55:50', '2024-08-11 21:55:50'),
+(3, 'fas fa-share-alt', 'Best Companies', 'The best and reputed worldwide companies registered here and so you will get the quality jobs.', '2024-08-11 21:57:21', '2024-08-11 21:57:21');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
